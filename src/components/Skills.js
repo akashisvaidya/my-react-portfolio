e5286e0e74c9a4ee813dfff30bd8b6cfe06516ed
“Aakash <akashisvaidya@gmail.com>
2022-11-13 11:34:27 +1100

    changed the navbar

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,19 +13,28 @@ export const Skills = () => {
           {/* <!-- icons --> */}
           <div class="icons">
             <div>
-              <i class="fa-brands fa-html5 "></i> HTML
+              <i class="fa-brands fa-html5 "></i>
+              <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML">
+                HTML
+              </a>
             </div>
             <div>
-              <i class="fa-brands fa-css3-alt "></i> CSS
+              <i class="fa-brands fa-css3-alt "></i>
+              <a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics">
+                CSS
+              </a>
             </div>
             <div>
-              <i class="fa-brands fa-square-js"></i> JavaScript
+              <i class="fa-brands fa-square-js"></i>{" "}
+              <a href="https://www.javascript.com/">JavaScript</a>
             </div>
             <div>
-              <i class="fa-brands fa-figma"></i>Figma
+              <i class="fa-brands fa-figma"></i>
+              <a href="https://www.figma.com/resources/learn-design/">Figma</a>
             </div>
             <div>
-              <i class="fab fa-react"></i>React
+              <i class="fab fa-react"></i>
+              <a href="https://reactjs.org/tutorial/tutorial.html">React</a>
             </div>
           </div>
         </div>
