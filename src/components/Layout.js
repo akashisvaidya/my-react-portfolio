cd47336351a4e95142de18382e7462c59b3e8188
“Aakash <akashisvaidya@gmail.com>
2022-11-13 15:13:47 +1100

    footer and nave bar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,11 @@
 import React from "react";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
     <>
       {children}
-      <footer class="p-4 text-center">
-        Copyright &copy; All Rights Reserved. Made with ❤️ by me 😊.
-      </footer>
-      <a id="goup" class="up" href="#navbar">
-        <i class="fa-solid fa-arrow-up"></i>
-      </a>
+      <Footer />
     </>
   );
 };
