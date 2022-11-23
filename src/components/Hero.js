dbe0a589b77715347736f836c8f6f5493c7f1cb9
akashisvaidya <akashisvaidya@gmail.com>
2022-11-23 20:44:41 +1100

    trying to change the colors

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import hero from "../assests/hero.jpg";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 export const Hero = () => {
   return (
     <>
@@ -19,26 +19,26 @@ export const Hero = () => {
                 Hi, I'm <span class="fw-bold">Aakash Vaidya</span>
               </h2>
               <div class="mt-2 mb-4 fw-bold">
-                <motion.h1
+                <h1
                   className="display-2"
                   initial={{ y: -100 }}
                   animate={{ scale: 1.5, y: 0 }}
                   transition={{ duration: 5 }}
                 >
                   Software Engineer
-                </motion.h1>
+                </h1>
               </div>
               <p>
                 I love coding and problem solving, and always looking for
                 opportunities to learn more and enhance skills.
               </p>
-              <motion.button
+              <button
                 whileHover={{ scale: 1.6 }}
                 class="btn btn-light mt-2"
                 id="btn"
               >
                 My Resume <i class="fa-solid fa-download"></i>
-              </motion.button>
+              </button>
             </div>
           </div>
         </div>
